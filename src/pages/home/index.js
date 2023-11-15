eabe82c0eb6f0834e2f922ecc241f39ba511bec7
Isaac Martins <111926496+ZaaankMJS@users.noreply.github.com>
2023-11-15 16:56:53 -0300

    Criando Elementos da Tela Home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import Header from '../../components/header/index.js';
+import Fundo from '../../assets/imgs/home/ImageBackground.png';
+import './styles.css';
 
 const Home = () => {
   return (
-    <Header/>
+    <>
+      <main className='background-page'>
+        <Header/>
+
+        <div className='img-div'>
+          <img src={Fundo} className='background-img'/>
+        </div>
+        <div className='text-div'>
+          <p className='text'>Uma nova forma de <br/> transformar seu trabalho <br/> único e elegante</p>
+        </div>
+
+      </main>
+    </>
   );
 }
 
