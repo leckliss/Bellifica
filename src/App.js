8e09759fd85fd9c6179cceb64378fde13d9bcfe1
LuanaArimitsu <142428639+LuanaArimitsu@users.noreply.github.com>
2023-11-17 23:31:49 -0300

    criando a página sobre nós

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Home from './pages/home';
+import Sobre from './pages/sobre/sobre';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      <Sobre/>
     </div>
   );
 }
---
+++ src/pages/sobre/sobre.js
@@ -0,0 +1,42 @@
+import Styles from './Sobre.module.css';
+import logo from '../../assets/imgs/sobre/Bellifica-logo.png';
+import make from '../../assets/imgs/sobre/moca-make.png';
+import cuidos from '../../assets/imgs/sobre/estetica.png';
+
+function Sobre() {
+    return (
+        <main className={Styles.fundo}>
+            <section className={Styles.quemsomos}>
+                <div className={Styles.img - logo}>
+                    <img src={logo} alt='logo' className={Styles.logo} />
+                </div>
+
+                <div className={Styles.fundao}>
+                    <div className={Styles.grupo1}>
+                        <img src={make} alt='uma mulher sendo maquiada' className={Styles.imagem1} />
+
+                        <div className={Styles.texto1}>
+                            <h1>Quem somos</h1>
+                            <p>Somos um grupo de estudantes de programação que se reuniu em Agosto de 2023, com o objetivo de desenvolver uma ferramenta capaz de auxiliar profissionais autônomos.</p>
+                        </div>
+                    </div>
+
+                    <div className={Styles.grupo2}>
+
+                        <div className={Styles.texto2}>
+                            <h1>Nosso objetivo</h1>
+                            <p>Auxiliar trabalhadores(as) autônomos simplificando a gestão de negócios para profissionais da beleza.<br /> Buscamos proporcionar ferramentas eficientes para agendamento e marketing, permitindo o crescimento e a excelência desses empreendedores.</p>
+                        </div>
+
+                        <img src={cuidos} alt='mulher se olhando no espelho' className={Styles.imagem2} />
+                    </div>
+
+                </div>
+            </section>
+
+
+        </main>
+    )
+}
+
+export default Sobre;
