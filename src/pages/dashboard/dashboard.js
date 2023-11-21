4ae33fb832b16e02cf638d567a8cb5e5cd8ff86f
leckliss <erickcaique150@hotmail.com>
2023-11-21 01:57:48 -0300

    comecando dashboard

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React,{useState} from 'react';
+import Bellifica from '../../assets/imgs/dashboard/bellifica.png';
+import BellificaB from '../../assets/imgs/dashboard/B.png';
+import './styles.css';
+//components
+import Painel from '../../components/painel/painel'
+import Cursos from '../../components/cursos/cursos';
+import Agenda from '../../components/agenda/agenda';
+import Configuracoes from '../../components/configuracoes/configuracoes';
+
 
 function Dashboard() {
-  
-    return (
-      <div>
+  const [clicked, setClicked] = useState(false);
+  const [logoSrc, setLogoSrc] = useState(Bellifica);
+
+  return (
+    <div className="dashboard-container">
+      <div className="sidebar">
+        <img src={logoSrc} alt="Logo" className={clicked ? 'logo2' : 'logo'} />
+        <ul>
+          <li><a href="#"><img src={require('../../assets/imgs/dashboard/Initial.png')}/> Pagina Inicial</a> </li>
+          <li><a href="#"><img src={require('../../assets/imgs/dashboard/Calendar.png')}/> Calendario</a> </li>
+          <li><a href="#"><img src={require('../../assets/imgs/dashboard/People.png')}/> Cursos e Dicas</a> </li>
+          <li><a href="#"><img src={require('../../assets/imgs/dashboard/Settings.png')}/> Configurações</a> </li>
+          <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+          <br /><br /><br /><br /><br />
+          <li><a href="#"><img src={require('../../assets/imgs/dashboard/Logout.png')}/> Sair</a> </li>
+          
+        </ul>
+      </div>
+      <div className="main-content">
+        //components que serão renderizados
         <h2>Painel de Controle</h2>
-  
-        {/* Gráfico de Vendas */}
         <div>
-          <h3>Gráfico de Vendas</h3>
-        </div>
-  
-        {/* Vídeo de exemplo do YouTube */}
-        <div>
+          <h3>Conteúdo Dinâmico</h3>
           <h3>Vídeo de Exemplo</h3>
           <iframe
             width="560"
@@ -25,7 +45,9 @@ function Dashboard() {
           ></iframe>
         </div>
       </div>
-    );
-  }
-  
-  export default Dashboard;
+    </div>
+
+  );
+}
+
+export default Dashboard;
