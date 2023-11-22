a627589f558e73b927692e67deb6c4c070d69091
Matheus José <matheusjose1242@gmail.com>
2023-11-22 00:13:32 -0300

    desenvolvendo fale conosco

--- src/pages/faleconosco/faleconosco.js
+++ src/pages/faleconosco/faleconosco.js
@@ -1,12 +1,13 @@
 import Header from "../../components/header";
 import Footer from "../../components/footer";
+import Style from "./faleconosco.module.css";
 
 function Faleconosco() {
     return(
         <>
         <Header/>
-        <section>
-
+        <section className={Style.faleconosco_corpo}>
+        <h1>teste</h1>
         </section>
         <Footer/>
         </>
