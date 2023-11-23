c95f3091e7153fc8990f71954862dde3e878b24b
Matheus José <matheusjose1242@gmail.com>
2023-11-23 00:45:06 -0300

    Fale conosco completa

--- src/pages/faleconosco/faleconosco.js
+++ src/pages/faleconosco/faleconosco.js
@@ -1,13 +1,20 @@
 import Header from "../../components/header";
 import Footer from "../../components/footer";
 import Style from "./faleconosco.module.css";
+import {AiOutlineSend} from "react-icons/ai"
 
 function Faleconosco() {
     return(
         <>
         <Header/>
         <section className={Style.faleconosco_corpo}>
-        <h1>teste</h1>
+        <div className={Style.formulariofaleconosco_corpo}>
+            <h2 className={Style.titulo_formulariofaleconosco}>Fale conosco</h2>
+            <input className={Style.input_nomefaleconosco} type="name" placeholder="Seu nome" required></input>
+            <input className={Style.input_emailfaleconosco2} type="email" placeholder="Seu email" required></input>
+            <textarea className="input_textfaleconosco" placeholder="Seu comentario" lenght="50"></textarea>
+            <button className={Style.submit_faleconosco} type="submit"><span className={Style.send_faleconosco}>Enviar</span> <AiOutlineSend className={Style.AiOutlineSend}/></button>
+        </div>
         </section>
         <Footer/>
         </>
