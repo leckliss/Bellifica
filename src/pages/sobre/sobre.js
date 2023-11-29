4fc9d742be44baba4b59ea5dda39fdb3c11ac540
LuanaArimitsu <142428639+LuanaArimitsu@users.noreply.github.com>
2023-11-28 22:37:49 -0300

    alterando pagina sobre

--- src/pages/sobre/sobre.js
+++ src/pages/sobre/sobre.js
@@ -16,12 +16,9 @@ function Sobre() {
 
             <Header />
 
-             {/*parte de quem somos*/}
+            {/*parte de quem somos*/}
 
             <section className={Styles.quemsomos}>
-                <div className={Styles.imglogo}>
-                    <img src={logo} alt='logo' className={Styles.logo} />
-                </div>
 
                 <div className={Styles.fundao}>
                     <div className={Styles.grupo1}>
@@ -38,7 +35,7 @@ function Sobre() {
                         <div className={Styles.texto2}>
                             <h2 className={Styles.objetivo}>Nosso objetivo</h2>
                             <p className={Styles.textobjetivo}>Auxiliar trabalhadores(as) autônomos simplificando a gestão de negócios para profissionais da beleza.<br /> Buscamos proporcionar ferramentas eficientes para agendamento e marketing, permitindo o crescimento e a excelência desses empreendedores.</p>
-                            <h1 className={Styles.titprinci}>Nossos Princípios</h1>
+
                         </div>
 
                         <img src={cuidos} alt='mulher se olhando no espelho' className={Styles.imagem2} />
@@ -49,38 +46,48 @@ function Sobre() {
             {/*parte dos principios*/}
 
             <section className={Styles.princi}>
-                <div className={Styles.enfoque}>
-                    <div className={Styles.conteuenfo}>
-                        <h2 className={Styles.enfo}>Enfoque</h2>
-                        <p className={Styles.textenfo}>Simplificar a gestão e fomentar o crescimento dos empreendedores da beleza através de soluções tecnológicas inovadoras.</p>
-                    </div>
-                    <img src={enfoque} alt='um olho desenhado' className={Styles.iconeenf} />
-                </div>
+                <h1 className={Styles.titprinci}>Nossos Princípios</h1>
+                
+                <div className={Styles.princitudo}>
+                    <div className={Styles.campenfoque}>
+                        <div className={Styles.enfoque}>
 
-                <div className={Styles.campetica}>
-                    <div className={Styles.etica}>
-                        <img src={etica} alt='um coracao' className={Styles.iconeetic} />
-                        <div className={Styles.conteuetica}>
-                            <h2 className={Styles.etic}>Ética</h2>
-                            <p className={Styles.textetic}>Comprometidos em facilitar a vida dos profissionais autônomos da beleza por meio de eficiência, excelência e inovação tecnológica.</p>
+                            <div className={Styles.conteuenfo}>
+                                <h2 className={Styles.enfo}>Enfoque</h2>
+                                <p className={Styles.textenfo}>Simplificar a gestão e fomentar o crescimento dos empreendedores da beleza através de soluções tecnológicas inovadoras.</p>
+                            </div>
+
+                            <img src={enfoque} alt='um olho desenhado' className={Styles.iconeenf} />
+                        </div>
+                    </div>
+
+                    <div className={Styles.campoetica}>
+                        <div className={Styles.campetica}>
+
+                            <div className={Styles.etica}>
+                                <img src={etica} alt='um coracao' className={Styles.iconeetic} />
+                                <div className={Styles.conteuetica}>
+                                    <h2 className={Styles.etic}>Ética</h2>
+                                    <p className={Styles.textetic}>Comprometidos em facilitar a vida dos profissionais autônomos da beleza por meio de eficiência, excelência e inovação tecnológica.</p>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={Styles.campmeta}>
+                        <div className={Styles.meta}>
+                            <div className={Styles.conteumeta}>
+                                <h2 className={Styles.met}>Meta</h2>
+                                <p className={Styles.textmet}>Desenvolver ferramentas acessíveis e eficazes para a gestão de negócios de beleza, impulsionando o sucesso e a autonomia dos profissionais.</p>
+                            </div>
+                            <img src={meta} alt='um tiro ao alvo' className={Styles.iconemeta} />
                         </div>
                     </div>
                 </div>
-
-                <div className={Styles.meta}>
-                    <div className={Styles.conteumeta}>
-                        <h2 className={Styles.met}>Meta</h2>
-                        <p className={Styles.textmet}>Desenvolver ferramentas acessíveis e eficazes para a gestão de negócios de beleza, impulsionando o sucesso e a autonomia dos profissionais.</p>
-                    </div>
-                    <img src={meta} alt='um tiro ao alvo' className={Styles.iconemeta} />
-                </div>
             </section>
 
-            <section className={Styles.bellogo}>
-            <img src={bellogo} alt='logo da Bellifica e seus siginificados' className={Styles.siglogo} />
-            </section>
-
-            <Footer/>
+            <Footer />
         </body>
 
     )
