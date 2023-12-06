2d5f917360bb03744ed859a8d8dd24f7efc5989c
LuanaArimitsu <142428639+LuanaArimitsu@users.noreply.github.com>
2023-12-06 19:49:40 -0300

    finalizando carroseis pag cursos

---
+++ src/pages/curso/ImageCarousel.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import Styles from './ImageCarousel.module.css'; // Importe o arquivo de estilos
+
+function ImageCarousel({ images }) {
+    const [startIndex, setStartIndex] = useState(0);
+
+    const nextSet = () => {
+        const newIndex = startIndex + 3;
+        setStartIndex(newIndex >= images.length ? 0 : newIndex);
+    };
+
+    const prevSet = () => {
+        const newIndex = startIndex - 3;
+        setStartIndex(newIndex < 0 ? images.length - 3 : newIndex);
+    };
+
+    return (
+        <div className={Styles.carouselContainer}>
+        <button onClick={prevSet} disabled={startIndex === 0}>
+          Previous
+        </button>
+        <div className={Styles.carousel}>
+          {images.slice(startIndex, startIndex + 3).map((image, index) => (
+            <a key={startIndex + index} href={image.link} className={Styles.imageLink}>
+              <img
+                src={image.src}
+                alt={`Image ${startIndex + index}`}
+                className={Styles.image}
+              />
+              <p className={Styles.imageDescription}>{image.description}</p>
+            </a>
+          ))}
+        </div>
+        <button onClick={nextSet} disabled={startIndex + 3 >= images.length}>
+          Next
+        </button>
+      </div>
+    );
+}
+
+export default ImageCarousel;
--- src/pages/curso/curso.js
+++ src/pages/curso/curso.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import ImageCarousel from './ImageCarousel';
+
 
 import Styles from './Curso.module.css';
 import Header from '../../components/header/index'
@@ -6,38 +8,143 @@ import Footer from '../../components/footer/index';
 
 import blob from '../../assets/imgs/curso/mulher-estudando.png'
 import senac from '../../assets/imgs/curso/logo-senac.png'
-import financeiro from '../../assets/imgs/curso/financeiro.jpg'
-import marketing from '../../assets/imgs/curso/marketing.jpg'
-import estetica from '../../assets/imgs/curso/beleza-estetica.jpg'
+import financeiro from '../../assets/imgs/curso/financeiro.png'
+import marketing from '../../assets/imgs/curso/marketing.png'
+import estetica from '../../assets/imgs/curso/beleza-estetica.png'
 import cabelosenac from '../../assets/imgs/curso/cabelo-senac.png'
-import maquiadorasenac from '../../assets/imgs/curso/maquiadora.jpg'
-import manicuresenac from '../../assets/imgs/curso/maos-pes.jpg'
+import maquiadorasenac from '../../assets/imgs/curso/maquiadora.png'
+import manicuresenac from '../../assets/imgs/curso/maos-pes.png'
+
+import embelleze from '../../assets/imgs/curso/embelleze-logo.png'
+import cabembelleze from '../../assets/imgs/curso/cabelo-embelleze.png'
+import barbaembelleze from '../../assets/imgs/curso/barba-embelleze.png'
+import olhosembelleze from '../../assets/imgs/curso/olhos-embelleze.png'
+import makeembelleze from '../../assets/imgs/curso/maquiagem-embelleze.png'
+import unhasembelleze from '../../assets/imgs/curso/unhas-embelleze.png'
+import depiembelleze from '../../assets/imgs/curso/depilação-embelleze.png'
+
+import boti from '../../assets/imgs/curso/logo-boticario.png'
+import unhasboti from '../../assets/imgs/curso/unhas-boticario.png'
+import alongamentoboti from '../../assets/imgs/curso/alongamento-boticario.png'
+import makeboti from '../../assets/imgs/curso/maquiagem-boticario.png'
+import vendasboti from '../../assets/imgs/curso/vendas-boticario.png'
+import desenboti from '../../assets/imgs/curso/desenvolvimento-pessoal-boticario.png'
+import empreenboti from '../../assets/imgs/curso/mulher-empreendedora-boticario.png'
 
 
 
 
 function Curso() {
 
-    const [currentCardIndex, setCurrentCardIndex] = useState(0);
+    // Array de imagens
+    const images = [
+        {
+            src: financeiro,
+            description: 'Financeiro',
+            link: 'https://www.sp.senac.br/areas/gestao-e-negocios/financas?utm_source=google&utm_medium=cpc&utm_campaign=GCR_Areas&utm_content=Sitelink&gad_source=1&gclid=CjwKCAiA1MCrBhAoEiwAC2d64Wb-ejO6IrS-99tZ73oVkKENp5Ew2W_O3fQwiPirD36vTiWBahk-GBoC_JMQAvD_BwE'
+        },
 
-    const cards = [
-        { image: financeiro, description: 'Curso de Finanças' },
-        { image: marketing, description: 'Curso de Marketing' },
-        { image: estetica, description: 'Curso de Beleza e Estética' },
-        { image: cabelosenac, description: 'Curso de Cabelo' },
-        { image: maquiadorasenac, description: 'Curso de Beleza e Estética' },
-        { image: manicuresenac, description: 'Curso de Beleza e Estética' },
+        {
+            src: marketing,
+            description: 'Marketing',
+            link: 'https://www.sp.senac.br/areas/comunicacao-e-marketing/marketing'
+        },
+        {
+            src: estetica,
+            description: 'Estética',
+            link: 'https://www.sp.senac.br/areas/beleza-e-estetica/estetica?gad_source=1&gclid=CjwKCAiAjrarBhAWEiwA2qWdCAbomlYHeid3IPC-hf2k5EARQwI9DeXpXcxjogIml_wjlCoROhBrNhoCvIwQAvD_BwE#cursos-tecnicos?utm_source=google&utm_medium=cpc&utm_campaign=GCR_Areas&utm_content=Sitelink'
+        },
+        {
+            src: cabelosenac,
+            description: 'Cabelo e Barbearia',
+            link: 'https://www.sp.senac.br/areas/beleza-e-estetica/cabelo-e-barbearia?gad_source=1&gclid=CjwKCAiAjrarBhAWEiwA2qWdCAbomlYHeid3IPC-hf2k5EARQwI9DeXpXcxjogIml_wjlCoROhBrNhoCvIwQAvD_BwE#cursos-tecnicos?utm_source=google&utm_medium=cpc&utm_campaign=GCR_Areas&utm_content=Sitelink'
+        },
+        {
+            src: maquiadorasenac,
+            description: 'Maquiagem',
+            link: 'https://www.sp.senac.br/areas/beleza-e-estetica/maquiagem?gad_source=1&gclid=CjwKCAiAjrarBhAWEiwA2qWdCAbomlYHeid3IPC-hf2k5EARQwI9DeXpXcxjogIml_wjlCoROhBrNhoCvIwQAvD_BwE#cursos-tecnicos?utm_source=google&utm_medium=cpc&utm_campaign=GCR_Areas&utm_content=Sitelink'
+        },
+        {
+            src: manicuresenac,
+            description: 'Mãos e Pés',
+            link: 'https://www.sp.senac.br/areas/beleza-e-estetica/pes-e-maos?gad_source=1&gclid=CjwKCAiAjrarBhAWEiwA2qWdCAbomlYHeid3IPC-hf2k5EARQwI9DeXpXcxjogIml_wjlCoROhBrNhoCvIwQAvD_BwE#cursos-tecnicos?utm_source=google&utm_medium=cpc&utm_campaign=GCR_Areas&utm_content=Sitelink'
+        },
+
     ];
 
-    const nextCard = () => {
-        const newIndex = (currentCardIndex + 1) % cards.length;
-        setCurrentCardIndex(newIndex);
-    };
+    const images2 = [
+        {
+            src: cabembelleze,
+            description: 'Cabelo',
+            link: 'https://institutoembelleze.com/pt-br/categoria/cabelo'
+        },
 
-    const prevCard = () => {
-        const newIndex = (currentCardIndex - 1 + cards.length) % cards.length;
-        setCurrentCardIndex(newIndex);
-    };
+        {
+            src: barbaembelleze,
+            description: 'Barba',
+            link: 'https://institutoembelleze.com/pt-br/categoria/barba'
+        },
+
+        {
+            src: olhosembelleze,
+            description: 'Olhos',
+            link: 'https://institutoembelleze.com/pt-br/categoria/olhar'
+        },
+
+        {
+            src: makeembelleze,
+            description: 'Maquiagem',
+            link: 'https://institutoembelleze.com/pt-br/categoria/maquiagem'
+        },
+
+        {
+            src: unhasembelleze,
+            description: 'Unhas',
+            link: 'https://institutoembelleze.com/pt-br/categoria/unhas'
+        },
+
+        {
+            src: depiembelleze,
+            description: 'Depilação',
+            link: 'https://institutoembelleze.com/pt-br/categoria/depilacao'
+        },
+
+    ];
+
+    const images3 = [
+        {
+            src: unhasboti,
+            description: 'Unhas'
+        },
+
+        {
+            src: alongamentoboti,
+            description: 'Alongamento de Unhas'
+        },
+
+        {
+            src: makeboti,
+            description: 'Maquiagem'
+        },
+
+        {
+            src: vendasboti,
+            description: 'Vendas'
+        },
+
+        {
+            src: desenboti,
+            description: 'Desenvolvimento pessoal'
+        },
+
+        {
+            src: empreenboti,
+            description: 'Empreendedorismo'
+        },
+
+    ];
+
+
 
     return (
 
@@ -62,18 +169,26 @@ function Curso() {
             </section>
 
             <section className={Styles.senac}>
-                <img src={senac} alt='logo senac' className={Styles.logosenac} />
 
-                <div className={Styles.cardCarousel}>
-                <button onClick={prevCard}>Previous</button>
-                <div className={Styles.card}>
-                    <img src={cards[currentCardIndex].image} alt={cards[currentCardIndex].description} />
-                    <p className={Styles.descri}>{cards[currentCardIndex].description}</p>
+                <div className={Styles.camplogosenac}>
+                    <img src={senac} alt='logo senac' className={Styles.logosenac} />
                 </div>
-                <button onClick={nextCard}>Next</button>
-            </div>
 
+                <div className={Styles.page}>
+                    <ImageCarousel images={images} />
+                </div>
 
+                <img src={embelleze} alt='logo embelleze' className={Styles.logoembelleze} />
+
+                <div className={Styles.carroselembelleze}>
+                    <ImageCarousel images={images2} />
+                </div>
+
+                <img src={boti} alt='logo instituto boticario' className={Styles.logoboti} />
+
+                <div className={Styles.carroselembelleze}>
+                    <ImageCarousel images={images3} />
+                </div>
             </section>
 
 
