f7cdb63a69d626461e238c57538ed996cacccb53
LuanaArimitsu <142428639+LuanaArimitsu@users.noreply.github.com>
2023-12-13 20:33:57 -0300

    altrações no carrosel

--- src/pages/curso/ImageCarousel.js
+++ src/pages/curso/ImageCarousel.js
@@ -21,7 +21,9 @@ function ImageCarousel({ images }) {
   return (
     <div className={Styles.carouselContainer}>
       <button onClick={prevSet} disabled={startIndex === 0}>
-        Anterior
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#000" class="bi bi-arrow-left" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8" />
+        </svg>
       </button>
       <div className={Styles.carousel}>
         {images.slice(startIndex, startIndex + 3).map((image, index) => (
@@ -44,7 +46,9 @@ function ImageCarousel({ images }) {
         ))}
       </div>
       <button onClick={nextSet} disabled={startIndex + 3 >= images.length}>
-        Próximo
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#000" class="bi bi-arrow-right" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
+        </svg>
       </button>
     </div>
   );
