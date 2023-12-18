4892b5ecdf6e791ae7531ad50eab7c96f53dd008
leckliss <erickcaique150@hotmail.com>
2023-12-18 08:37:37 -0300

    alterando3 protected routes

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { Route, Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 const isAuthenticated = () => {
     const token = localStorage.getItem('authToken');
     return token != null;
 };
 
-const ProtectedRoute = ({ element: Component, ...rest }) => (
-    <Route
-        {...rest}
-        element={isAuthenticated() ? Component : <Navigate to="/login" replace />}
-    />
-);
+const ProtectedRoute = ({ children }) => {
+    return isAuthenticated() ? children : <Navigate to="/login" replace />;
+};
 
 export default ProtectedRoute;
